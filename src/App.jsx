2c0899ed70d0b1802a442a7561669ee4d732e74d
amal <amal95salah@gmail.com>
2022-12-21 22:27:10 +0200

    Feat: Built project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
-import Footer from "./components/Footer";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<Home />} />
+          <Route path="/contact" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
       <Footer />
     </div>
   );
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Footer from '../../components/Footer/Footer';
+
+function Home() {
+  return (
+    <div className="Home">
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
