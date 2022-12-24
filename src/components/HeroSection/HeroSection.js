909caf020d12e63799f6371b3cc086d28c2cd0c0
Ali ibrahim <ali.ibrahim.jasim@gmail.com>
2022-12-24 09:53:32 +0300

    close 4-create-hero-section

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -12,11 +12,11 @@ function HeroSection() {
         <div className='flex justify-center items-center flex-wrap-reverse flex-row-reverse  '>
           <img className=' w-3/5 h-[425px] ml-auto  z-50  pt-10 ' src={therapist} alt='therapist talking to a patient ' />
           <div className='w-40 text-center  z-50 grid items-center justify-center pt-40 ml-60'>
-            <p className=' text-center'>WE ARE HERE TO </p>
-            <span className=''>HELP</span>
+            <p className=' text-4xl '>WE ARE HERE TO </p>
+            <span className='text-9xl'>HELP</span>
             <button
             type="button"
-            className=" text-2xl text-black font-semibold  w-96  border border-indigo-300 bg-cyan-500  rounded-md px-6 py-4 m-5 transition duration-500 ease select-none hover:bg-cyan-600 focus:outline-none focus:shadow-outline  "
+            className=" text-2xl  mt-10  text-black font-semibold  w-96  border border-indigo-300 bg-cyan-500  rounded-md px-10 py-4 m-5 transition duration-500 ease select-none hover:bg-cyan-600 focus:outline-none focus:shadow-outline  "
           >BOOK AN APPOINTMENT</button>
           </div>
 
