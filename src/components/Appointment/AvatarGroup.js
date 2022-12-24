4a8a89f15119d6d4d82bdc32f85f561953bd398d
Hevar Tofiq Hama <rageofkurd98@gmail.com>
2022-12-24 19:04:33 +0300

    subject: Appointment section made responsive
    body: it's iniciated inside Home Component that been made responsive.

--- src/components/Appointment/AvatarGroup.js
+++ src/components/Appointment/AvatarGroup.js
@@ -1,37 +1,38 @@
+
 import { Avatar} from 'flowbite-react/lib/esm/components';
 
-function AvatarGroup({size}){
+function AvatarGroup(){
 
     return( <Avatar.Group  >
       <Avatar
         img="https://flowbite.com/docs/images/people/profile-picture-1.jpg"
         rounded
         stacked
-       size={size}
+       size="lg"
       />
       <Avatar
         img="https://flowbite.com/docs/images/people/profile-picture-2.jpg"
         rounded
         stacked
-        size={size}
+        size="lg"
       />
       <Avatar
         img="https://flowbite.com/docs/images/people/profile-picture-3.jpg"
         rounded
         stacked
-        size={size}
+        size="lg"
       />
       <Avatar
         img="https://flowbite.com/docs/images/people/profile-picture-4.jpg"
         rounded
         stacked
-        size={size}
+        size="lg"
       />
       <Avatar
         img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
         rounded
         stacked
-        size={size}
+        size="lg"
       />
     </Avatar.Group>);
   }
