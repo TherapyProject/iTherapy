b50c5300d7b004f32bdc1434335f4ad0f4c9762d
Ali ibrahim <ali.ibrahim.jasim@gmail.com>
2022-12-25 18:47:03 +0300

    Add The first part of the about page HEALING

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -3,7 +3,22 @@ import React from 'react';
 function About() {
   return (
     <div className="About">
-      <h1>About</h1>
+      <div className="flex flex-col px-52 py-14 md:text-left sm:text-center ">
+        <h1 className="font-[Poppins] text-4xl sm:text-5xl	 ">HEALING!</h1>
+        <h3 className="font-[Poppins] text-slate-400 pb-10 text-2xl whitespace-nowrap">
+          some cool one liner !
+        </h3>
+        <p className="font-[Poppins]  md:text-lg leading-7	font-normal break-normal">
+          At Healing &#39;we believe there is a better way to do things. A more
+          valuable way where customers are earned rather than bought. We&#39;re
+          obsessively passionate about it&#39; and our mission is to help people
+          achieve it. We focus on search engine optimization. It &#39;s one of
+          the least understood and least transparent aspects of great
+          marketing&#39; and we see that as an opportunity. We&#39;re excited to
+          simplify SEO for everyone through our software, education, and
+          community.
+        </p>
+      </div>
     </div>
   );
 }
