ecc588694d4bee8be2b9df4e0b73fdcaba2f5f1c
amal <amal95salah@gmail.com>
2022-12-26 00:04:58 +0200

    change font style

--- src/components/Purchase/Purchase.js
+++ src/components/Purchase/Purchase.js
@@ -2,32 +2,32 @@ import React from "react";
 import PurchaseCard from "./PurchaseCard";
 
 function Purchase() {
-    return (
-    <header className="p-10">
-  <div>
-    <span className="text-5xl font-bold ">PURCHASE TICKETS</span>
-    <br/>
-    <br/>
-    <span className="text-2xl font-bold">PURCHASE TICKETS THAT CAN BE USED TO BOOK APPOINTMENTS!</span>
-  </div>
-    <div className="flex flex-wrap gap-5 justify-around mt-10">
-      <PurchaseCard title="1 TICKET" price="10"/>
-      <PurchaseCard title="5 TICKETS" price="40"/>
-      <PurchaseCard title="10 TICKETS" price="70"/>
-    </div>
-    <div className="mt-10">
-      <div className="card w-500 bg-base-100 shadow-xl">
-        <div className="card-body">
-          <h2 className="card-title text-3xl justify-center">ARE YOU A COUNSELOR?</h2>
-          <p className="text-xl justify-center">Interested in joining our mental health platform? You decide your schedule and how much you want to work, we’ll take care of the client referrals and billing details!</p>
-          <div className="card-actions justify-center">
-            <button type="button" className="btn btn-info">LEARN MORE</button>
+  return (
+    <header className="pb-14 pt-14 md:pl-10 lg:pl-20 pl-7 pr-7 md:pr-16 ">
+      <div>
+        <span className=" font-['Poppins'] text-5xl font-bold ">PURCHASE TICKETS</span>
+        <br />
+        <br />
+        <span className="font-['Poppins'] text-2xl font-bold">PURCHASE TICKETS THAT CAN BE USED TO BOOK APPOINTMENTS!</span>
+      </div>
+      <div className="flex flex-wrap gap-5 justify-around mt-10">
+        <PurchaseCard title="1 TICKET" price="10" />
+        <PurchaseCard title="5 TICKETS" price="40" />
+        <PurchaseCard title="10 TICKETS" price="70" />
+      </div>
+      <div className="mt-10">
+        <div className="card w-500 bg-base-100 shadow-xl">
+          <div className="card-body">
+            <h2 className="font-['Poppins']  card-title text-3xl justify-center">ARE YOU A COUNSELOR?</h2>
+            <p className="font-['Poppins']  text-xl justify-center">Interested in joining our mental health platform? You decide your schedule and how much you want to work, we’ll take care of the client referrals and billing details!</p>
+            <div className="card-actions justify-center">
+              <button type="button" className="font-['Poppins']  btn btn-info">LEARN MORE</button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </header>
-    );
+    </header>
+  );
 }
 
 export default Purchase;
--- src/components/Purchase/PurchaseCard.js
+++ src/components/Purchase/PurchaseCard.js
@@ -1,14 +1,16 @@
 
-function PurchaseCard({title , price}) {
-	return(<div className="card w-70 bg-base-100 shadow-xl">
-	<div className="card-body">
-		<h2 className="card-title text-3xl justify-center">{title}</h2>
-		<span className="text-xl text-center">{price}$</span>
-		<div className="card-actions justify-center">
-			<button type="button" className="btn btn-info">PURCHASE</button>
+function PurchaseCard({ title, price }) {
+	return (
+		<div className="card w-70 bg-base-100 shadow-xl">
+			<div className="font-['Poppins'] card-body">
+				<h2 className="card-title text-3xl justify-center">{title}</h2>
+				<span className="text-xl text-center">{price}$</span>
+				<div className="card-actions justify-center">
+					<button type="button" className="btn btn-info">PURCHASE</button>
+				</div>
+			</div>
 		</div>
-	</div>
-</div>);
+	);
 }
 
 export default PurchaseCard;
