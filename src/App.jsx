0b12209d113b290a722bea2426993bb4c2d4806e
amal <amal95salah@gmail.com>
2022-12-25 13:23:10 +0200

    create card components

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Navbar from './components/NavBar/Navbar';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App h-screen	w-full	flex flex-col">
       <Navbar />
       <BrowserRouter>
         <Routes>
