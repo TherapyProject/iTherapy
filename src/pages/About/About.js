7e5a827effb2f2a6e4e1dfe4f3f63cd17e67f004
Ali ibrahim <ali.ibrahim.jasim@gmail.com>
2022-12-26 22:15:06 +0300

    commit changes 2

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -2,13 +2,16 @@ import React from 'react';
 
 function About() {
   return (
-    <div className="About">
-      <div className="flex flex-col px-52 py-14 md:text-left sm:text-center ">
-        <h1 className="font-[Poppins] text-4xl sm:text-5xl	 ">HEALING!</h1>
-        <h3 className="font-[Poppins] text-slate-400 pb-10 text-2xl whitespace-nowrap">
+    <div className="About  ">
+      <div className="flex flex-col px-20 py-14 sm:px-28 sm:text-center md:text-left  ">
+        <h1 className="font-[Poppins] text-4xl sm:text-5xl	text-center sm:text-left">
+          HEALING!
+        </h1>
+        <h3 className="font-[Poppins] text-slate-400 pb-10 text-2xl whitespace-nowrap text-center sm:text-left">
           some cool one liner !
         </h3>
-        <p className="font-[Poppins]  md:text-lg leading-7	font-normal break-normal">
+
+        <p className="  font-[Poppins]   md:text-lg leading-7	font-normal min-w-[250px] px-3 m-7 sm:px-0 sm:m-0	sm:w-[1000px] text-center sm:text-left ">
           At Healing &#39;we believe there is a better way to do things. A more
           valuable way where customers are earned rather than bought. We&#39;re
           obsessively passionate about it&#39; and our mission is to help people
@@ -18,6 +21,10 @@ function About() {
           simplify SEO for everyone through our software, education, and
           community.
         </p>
+      </div>
+      <div className="bg-teal-50  w-full h-[400px] mt-32">
+
+        hello
       </div>
     </div>
   );
