1d820cd5ec673bf6608da0426bbc6b3b2abd592a
Ali Ibrahim <113296754+bluesky1992-web@users.noreply.github.com>
2023-01-02 20:49:24 +0300

    Bluesky1992-web/issue29 (#46)
    * Add The first part of the about page HEALING
    
    * commit changes 2
    
    * add the second section in about page
    
    * enhance the font size and readability and the responsiveness
    
    * ready to be merged
    
    Co-authored-by: Hevar Tofiq Hama <rageofkurd98@gmail.com>

--- src/components/Appointment/Appointment.js
+++ src/components/Appointment/Appointment.js
@@ -1,10 +1,7 @@
-
 import { Button } from 'flowbite-react/lib/esm/components';
 import AvatarGroup from './AvatarGroup';
 
-
 function Appointment() {
-
   return (
     <div className=" md:pl-10 lg:pl-20  w-full h-[490px] flex flex-col md:-gap-10  justify-evenly sm:justify-around items-start p-5 bg-[#FEE89E]">
       <div className="w-full p-5 flex flex-row ">
@@ -17,7 +14,16 @@ function Appointment() {
         <AvatarGroup size='lg' />
       </div>
 
-      <div className='px-5 sm:pr-28 md:pr-56'><p className=' text-sm sm:text-base lg:text-lg text-justify  text-gray-600 font-medium'>Tap into the worlds largest network of licensed, accredited, and experienced therapists who can help you with a range of issues including depression, anxiety, relationships, trauma, grief, and more. with our therapists, you get the same professionalism and quality you would expect from an in-office therapist, but with the ability to communicate whenever and however you want.</p></div>
+      <div className="px-5 sm:pr-28 md:pr-56">
+        <p className=" text-sm sm:text-base lg:text-lg text-justify  text-gray-600 font-medium">
+          Tap into the worlds largest network of licensed, accredited, and
+          experienced therapists who can help you with a range of issues
+          including depression, anxiety, relationships, trauma, grief, and more.
+          with our therapists, you get the same professionalism and quality you
+          would expect from an in-office therapist, but with the ability to
+          communicate whenever and however you want.
+        </p>
+      </div>
 
       <div className='pl-4'>
         <Button className='sm:w-48 md:w-52 lg:w-64 xl:w-72' gradientDuoTone="purpleToBlue">
@@ -26,9 +32,5 @@ function Appointment() {
     </div>
   );
 }
-
-
-
-
 
 export default Appointment;
