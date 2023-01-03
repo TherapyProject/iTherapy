22f7f03d79a6ff70cf6ac79fe6f3099c1998a00f
Ali ibrahim <ali.ibrahim.jasim@gmail.com>
2023-01-03 21:10:59 +0300

    some bugs needs to be solved

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 import Image from '../../images/Image.png';
 
@@ -14,153 +13,156 @@ function Contact() {
         </p>
       </div>
       <div>
-        <h2 className="card-title text-center md:card-title mt-5 mb-10">
-          Type of contact
-        </h2>
-        <div className="text-justify md:flex justify-between">
-          <div className="flex flex-col text-xl gap-8 ">
+        <h2 className="text-xl text-center mt-8 mb-10">Type of contact</h2>
+        <div className="text-justify md:flex justify-between gap-8">
+          <div className="flex flex-col text-xl gap-8 mb-8">
             <div className="flex items-center mr-4">
-              <input
-                id="inline-radio"
-                type="radio"
-                value=""
-                name="inline-radio-group"
-                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-              />
               <label
                 htmlFor="inline-radio"
-                className="ml-2  font-medium text-black-400 dark:text-gray-500"
+                className="ml-2 text-black-400 dark:text-gray-500"
               >
+                <input
+                  id="inline-radio"
+                  type="radio"
+                  value=""
+                  name="inline-radio-group"
+                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                />
                 I have a question about the service.
               </label>
             </div>
             <div className="flex items-center mr-4">
-              <input
-                id="inline-2-radio"
-                type="radio"
-                value=""
-                name="inline-radio-group"
-                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-              />
               <label
                 htmlFor="inline-2-radio"
-                className="ml-2  font-medium text-black-400 dark:text-gray-500"
+                className="ml-2 text-black-400 dark:text-gray-500"
               >
+                <input
+                  id="inline-2-radio"
+                  type="radio"
+                  value=""
+                  name="inline-radio-group"
+                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                />
                 I&apos;m a registered client and I need support.
               </label>
             </div>
             <div className="flex items-center mr-4">
-              <input
-                checked
-                id="inline-checked-radio"
-                type="radio"
-                value=""
-                name="inline-radio-group"
-                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-              />
               <label
                 htmlFor="inline-checked-radio"
-                className="ml-2  font-medium text-black-400 dark:text-gray-500"
+                className="ml-2 text-black-400 dark:text-gray-500"
               >
+                <input
+                  checked
+                  id="inline-checked-radio"
+                  type="radio"
+                  value=""
+                  name="inline-radio-group"
+                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                />
                 I&apos;m a counselor interested in joining.
               </label>
             </div>
             <div className="flex items-center">
-              <input
-                id="inline-radio"
-                type="radio"
-                value=""
-                name="inline-radio-group"
-                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-              />
               <label
                 htmlFor="inline-disabled-radio"
-                className="ml-2  font-medium text-black-400 dark:text-gray-500"
+                className="ml-2 text-black-400 dark:text-gray-500"
               >
+                <input
+                  id="inline-radio"
+                  type="radio"
+                  value=""
+                  name="inline-radio-group"
+                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                />
                 I&apos;m a registered counselor and I need support.
               </label>
             </div>
             <div className="flex items-center">
-              <input
-                id="inline-radio"
-                type="radio"
-                value=""
-                name="inline-radio-group"
-                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-              />
               <label
                 htmlFor="inline-disabled-radio"
-                className="ml-2  font-medium text-black-400 dark:text-gray-500"
+                className="ml-2 text-black-400 dark:text-gray-500"
               >
+                <input
+                  id="inline-radio"
+                  type="radio"
+                  value=""
+                  name="inline-radio-group"
+                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                />
                 I have a business-related inquiry.
               </label>
             </div>
             <div className="flex items-center">
-              <input
-                id="inline-radio"
-                type="radio"
-                value=""
-                name="inline-radio-group"
-                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-              />
               <label
                 htmlFor="inline-disabled-radio"
-                className="ml-2  font-medium text-black-400 dark:text-gray-500"
+                className="ml-2 text-black-400 dark:text-gray-500"
               >
+                <input
+                  id="inline-radio"
+                  type="radio"
+                  value=""
+                  name="inline-radio-group"
+                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                />
                 I&apos;m interested in Healing Online for my organization.
               </label>
             </div>
             <div className="flex items-center">
-              <input
-                id="inline-radio"
-                type="radio"
-                value=""
-                name="inline-radio-group"
-                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-              />
               <label
                 htmlFor="inline-disabled-radio"
-                className="ml-2  font-medium text-black-400 dark:text-gray-500"
+                className="ml-2 text-black-400 dark:text-gray-500"
               >
+                <input
+                  id="inline-radio"
+                  type="radio"
+                  value=""
+                  name="inline-radio-group"
+                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                />
                 I have a billing related question.
               </label>
             </div>
           </div>
-          <div className="invisible md:visible">
+          <div className="hidden md:block">
             <img src={Image} alt="Contact Us" />
           </div>
         </div>
       </div>
 
-      <div className="flex">
-        <div className="flex flex-col md:grid grid-cols-2 space-x-52 ">
-          <div className="form-control w-full max-w-xs">
-            <label className="label">
-              <span className="card-title">Full Name:</span>
-            </label>
-            <input
-              type="text"
-              placeholder="Enter your full name here..."
-              className="input input-bordered w-full max-w-xs"
-            />
-            <div className="form-control w-full max-w-xs">
-              <label className="label">
-                <span className="card-title">Email:</span>
-              </label>
+      <div className="flex items-center justify-center">
+        <div className="flex w-full flex-wrap flex-row justify-center items-center sm:space-x-52">
+          <div className="form-control w-full max-w-sm">
+            <label className="label" htmlFor="name">
+              Full Name:
               <input
                 type="text"
-                placeholder="Enter your email address here..."
-                className="input input-bordered w-full max-w-xs"
+                id="name"
+                placeholder="Enter your full name here..."
+                className="input input-bordered w-full max-w-sm"
               />
-              <div className="form-control w-full max-w-xs">
-                <label className="label">
-                  <span className="card-title">Details:</span>
-                </label>
+            </label>
+
+            <div className="form-control w-full max-w-sm">
+              <label className="label" htmlFor="email">
+                Email:
                 <input
                   type="text"
-                  placeholder="Enter your details here..."
-                  className="input input-bordered w-full max-w-xs"
+                  id="email"
+                  placeholder="Enter your email address here..."
+                  className="input input-bordered w-full max-w-sm"
                 />
+              </label>
+
+              <div className="form-control w-full max-w-sm">
+                <label className="label" htmlFor="details">
+                  Details:
+                  <input
+                    type="text"
+                    id="details"
+                    placeholder="Enter your details here..."
+                    className="input input-bordered w-full max-w-sm	"
+                  />
+                </label>
                 <button className="btn btn-info mt-10" type="button">
                   SUBMIT
                 </button>
@@ -168,7 +170,7 @@ function Contact() {
             </div>
           </div>
 
-          <div className="justify-self-start md: card w-72 bg-teal-50 text-primary-content mt-10 p-5 ">
+          <div className="justify-self-start card bg-teal-50 text-primary-content mt-10 p-5 ">
             <h2 className="card-title text-black">Find Us At:</h2>
             <div className="card-body text-black">
               <p>Nergiz Plaza</p>
