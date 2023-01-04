9854f142eea3867434b86a967e2a0f10544f4bdf
Hevar Tofiq Hama <rageofkurd98@gmail.com>
2023-01-05 00:27:52 +0300

    Hero Section Made Responsive

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -7,28 +7,27 @@ function Hero() {
 
   return (
     <div
-      className=" font-['Poppins'] h-screen flex justify-center items-center md:flex-row flex-col md:pl-10 md:pb-40 lg:pl-20 bg-[url('components/HeroSection/HeroSection-images/linearbackground.png')]
+      className=" font-['Poppins'] h-screen flex justify-center items-center md:flex-row flex-col md:pl-10 md:gap-1 lg:gap-10 lg:pl-20 bg-cover bg-[url('components/HeroSection/HeroSection-images/linearbackground.png')]
     "
     >
-      <div className="flex flex-col items-center gap-14 ml-4">
-        <div className='flex flex-col items-center justify-center gap-5'>
-        <p className=" text-4xl ">
-          WE ARE HERE TO
-        </p>
-        <span className=" md:text-9xl text-7xl ">
-          HELP
-        </span>
+      <div className="flex flex-col items-center gap-14  ml-4">
+        <div className="flex flex-col items-center justify-center gap-5 font-semibold">
+          <p className=" text-4xl xl:text-5xl ">We Are Here To</p>
+          <span className=" text-7xl md:text-6xl lg:text-7xl xl:text-8xl ">
+            Help you
+          </span>
+        </div>
+
         <button
           type="button"
-          className=" md:py-2.5 md:px-1.5 md:mt-11 mt-6 text-black font-semibold border border-indigo-300 bg-cyan-500  rounded-md transition duration-500  select-none hover:bg-cyan-600 focus:outline-none focus:shadow-outline  "
-          onClick={() => navigate('/book')}
+          className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-base px-5 py-2.5 text-center  mb-2 "
         >
           BOOK AN APPOINTMENT
         </button>
       </div>
       <div className=" md:w-3/5 pt-10 pb-5 w-full">
         <img
-          className=" h-64 mx-auto"
+          className=" h-64 md:h-72 lg:h-80 xl:h-96 mx-auto"
           src={therapist}
           alt="therapist talking to a patient "
         />
