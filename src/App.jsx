44a80f3a753173beef55702ad738b60bb797a290
amal <amal95salah@gmail.com>
2023-01-09 19:29:16 +0200

    FEATURE: setup the firebase

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
 import Navbar from './components/NavBar/Navbar';
+import { AuthProvider } from './contexts/AuthContext';
 import About from './pages/About/About';
 import BlogsPage from './pages/Blogs/BlogsPage';
 import Contact from './pages/Contact/Contact';
@@ -14,19 +15,21 @@ function App() {
   return (
     <div className="App flex flex-col">
       <Navbar />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/blogs" element={<BlogsPage />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="/team" element={<Team />} />
-        </Routes>
-      </BrowserRouter>
-      <Footer />
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/blogs" element={<BlogsPage />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/team" element={<Team />} />
+          </Routes>
+        </BrowserRouter>
+        <Footer />
+      </AuthProvider>
     </div>
   );
 }
