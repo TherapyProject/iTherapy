4923aeda6734d79e539378699ed892ff789a50ee
rageofkurd <rageofkurd98@gmail.com>
2023-01-12 20:11:39 +0300

    home page overflowing issue been fixed

--- src/components/Appointment/Avatar.js
+++ src/components/Appointment/Avatar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Avatar = ({imgSrc}) => {
   return (
-     <img className=" h-24 sm:h-28 border-2 border-white rounded-full dark:border-gray-800" src={imgSrc} alt=""/>
+     <img className=" h-20 sm:h-28 border-2 border-white rounded-full dark:border-gray-800" src={imgSrc} alt=""/>
   )
 }
 
