6c2cc78692609b320b0a6507103e776b7d1d13f1
rageofkurd <rageofkurd98@gmail.com>
2023-01-16 20:09:49 +0300

    old card component been removed

--- src/pages/Blogs/BlogsPage.js
+++ src/pages/Blogs/BlogsPage.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import BlogCard from '../../components/BlogCard/BlogCard';
-import { Blogs } from '../../components/BlogCard/BlogMockData';
+
 
 function BlogsPage() {
   return (
@@ -8,18 +7,7 @@ function BlogsPage() {
       <h2 className="font-['Poppins'] text-lg sm:text-xl lg:text-2xl text-gray-700 font-bold mx-10 mt-5 ">
         Blogs:
       </h2>
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 mx-5 my-5 ">
-        {Blogs.map((blog) => (
-          <BlogCard
-            blogTitle={blog.title}
-            blogImg={blog.img}
-            authImg={blog.authImg}
-            authName={blog.authName}
-            authTitle={blog.authTitle}
-            minsRead={blog.minsRead}
-          />
-        ))}
-      </div>
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 mx-5 my-5 " />
     </div>
   );
 }
