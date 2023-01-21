442cb32aede2f2eb147bd4144a8ccf49382fe5c7
Aland Abdullah <32212842+alandio@users.noreply.github.com>
2023-01-21 20:57:16 +0300

    Fix: blogs carousel overflow

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
-        <div className="App flex flex-col bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
+        <div className="App flex flex-col bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 overflow-hidden">
           <Navbar />
           <Scroll />
           <Routes>
